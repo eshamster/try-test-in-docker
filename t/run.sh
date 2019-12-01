8dbf4e9fc9c0d2506508a6d8a3c494ceb2a2d6fe
eshamster <hamgoostar@gmail.com>
2019-12-01 19:00:03 +0900

    Fix container name

--- t/run.sh
+++ t/run.sh
@@ -4,4 +4,4 @@ set -eux
 
 base_dir=$(dirname ${0})
 cd ${base_dir}/..
-docker run -v $(pwd):/root/.roswell/local-projects/try-test-in-docker -t try-test
+docker run -v $(pwd):/root/.roswell/local-projects/try-test-in-docker -t test
