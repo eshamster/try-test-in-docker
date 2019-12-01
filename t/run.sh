bf892196ea11ce3a72bcd6fe771cbf2cfc040a2d
eshamster <hamgoostar@gmail.com>
2019-12-01 18:52:34 +0900

    Fix: Stop interactive run

--- t/run.sh
+++ t/run.sh
@@ -6,4 +6,4 @@ base_dir=$(dirname ${0})
 cd ${base_dir}
 docker build -t try-test .
 cd ..
-docker run -v $(pwd):/root/.roswell/local-projects/try-test-in-docker -ti try-test /bin/sh
+docker run -v $(pwd):/root/.roswell/local-projects/try-test-in-docker -t try-test
